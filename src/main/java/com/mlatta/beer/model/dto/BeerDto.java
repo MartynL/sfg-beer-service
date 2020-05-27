8f7307ff041fc2477e207f76bb1ba54f99fb71b4
mlatta <martyn.latta@bipsolutions.com>
2020-05-27 15:13:02 +0100

    Added validation annotations to BeerDto

--- src/main/java/com/mlatta/beer/model/dto/BeerDto.java
+++ src/main/java/com/mlatta/beer/model/dto/BeerDto.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import com.mlatta.beer.model.enums.BeerStyle;
 
 import lombok.AllArgsConstructor;
@@ -17,13 +22,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BeerDto {
 
+	@Null
 	private UUID id;
+	
+	@Null
 	private Integer version;
+	
+	@Null
 	private OffsetDateTime createdDate;
+	
+	@Null
 	private OffsetDateTime lastModified;
+	
+	@NotBlank
 	private String beerName;
+	
+	@NotNull
 	private BeerStyle beerStyle;
+	
+	@NotNull
+	@Positive
 	private Long upc;
+	
+	@NotNull
+	@Positive
 	private BigDecimal price;
 	private Integer quantityOnHand;
 	
