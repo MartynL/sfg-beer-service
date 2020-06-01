2af5e7cdd735fadf54da799eb24285810084d66e
mlatta <martyn.latta@bipsolutions.com>
2020-06-01 16:59:50 +0100

    implementing controller and fixing tests

--- src/main/java/com/mlatta/beer/controller/BeerController.java
+++ src/main/java/com/mlatta/beer/controller/BeerController.java
@@ -17,33 +17,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mlatta.beer.domain.Beer;
 import com.mlatta.beer.model.dto.BeerDto;
+import com.mlatta.beer.model.mappers.BeerMapper;
+import com.mlatta.beer.repositories.BeerRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 	
+	private final BeerMapper beerMapper;
+	private final BeerRepository beerRepository;
+	
 	@GetMapping("/{beerId}")
 	public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
-		//TODO impl
-		return ResponseEntity.ok(BeerDto.builder().build());
+		BeerDto beer = beerMapper.beerToBeerDto(beerRepository.findById(beerId).orElse(Beer.builder().build()));
+		return ResponseEntity.ok(beer);
 	}
 	
 	@PostMapping
 	public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
-		//TODO impl 
-		return ResponseEntity.created(URI.create("/api/v1/beer" + UUID.randomUUID())).build();
+		beerRepository.save(beerMapper.beerDtoToBeer(beerDto));
+		return ResponseEntity.created(URI.create("/api/v1/beer")).build();
 	}
 
 	@PutMapping("/{beerId}")
 	@ResponseStatus(NO_CONTENT)
 	public void updateBeerById(@PathVariable UUID beerId, @Validated @RequestBody BeerDto beerDto) {
-		//TODO impl
+		
+		beerRepository
+			.findById(beerId)
+			.ifPresent(b -> {
+				b.setBeerName(beerDto.getBeerName());
+				b.setBeerStyle(beerDto.getBeerStyle().toString());
+				b.setPrice(beerDto.getPrice());
+				b.setUpc(beerDto.getUpc());
+				
+				beerRepository.save(b);
+			});
+		
 	}
 	
 	@DeleteMapping("/{beerId}")
 	@ResponseStatus(NO_CONTENT)
 	public void deleteBeerById(@PathVariable UUID beerId) {
-		//TODO impl
+		beerRepository.deleteById(beerId);
 	}
 }
--- src/test/java/com/mlatta/beer/controller/BeerControllerTest.java
+++ src/test/java/com/mlatta/beer/controller/BeerControllerTest.java
@@ -1,5 +1,8 @@
 package com.mlatta.beer.controller;
 
+import static java.util.Optional.of;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -13,22 +16,32 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mlatta.beer.domain.Beer;
 import com.mlatta.beer.model.dto.BeerDto;
 import com.mlatta.beer.model.enums.BeerStyle;
+import com.mlatta.beer.repositories.BeerRepository;
 
 @WebMvcTest
 @ExtendWith(MockitoExtension.class)
+@ComponentScan("com.mlatta.beer.model.mappers")
 public class BeerControllerTest {
 
 	@Autowired MockMvc mockMvc;
 	@Autowired ObjectMapper objectMapper;
+	
+	@MockBean BeerRepository beerRepository;
 
 	@Test
 	void testGetBeerById() throws Exception {
+		
+		when(beerRepository.findById(any())).thenReturn(of(Beer.builder().build()));
+		
 		mockMvc
 			.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk());
