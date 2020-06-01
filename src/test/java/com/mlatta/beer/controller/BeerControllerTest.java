5b119d668b9e7eaaa948a5bede618c19ad028434
mlatta <martyn.latta@bipsolutions.com>
2020-06-01 14:00:34 +0100

    Adding mapstruct dependency

--- src/test/java/com/mlatta/beer/controller/BeerControllerTest.java
+++ src/test/java/com/mlatta/beer/controller/BeerControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.UUID;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
