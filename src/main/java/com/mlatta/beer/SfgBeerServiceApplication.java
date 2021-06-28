adf0be61f33a5b8fa29e11d44b0aa0a164e86548
Martyn Latta <mlatta1990@hotmail.co.uk>
2021-06-28 23:18:05 +0100

    configuring mysql for beer service

--- src/main/java/com/mlatta/beer/SfgBeerServiceApplication.java
+++ src/main/java/com/mlatta/beer/SfgBeerServiceApplication.java
@@ -3,9 +3,11 @@ package com.mlatta.beer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 
 @EnableCaching
-@SpringBootApplication
+@SpringBootApplication(exclude = {EurekaClientAutoConfiguration.class, EurekaDiscoveryClientConfiguration.class})
 public class SfgBeerServiceApplication {
 
 	public static void main(String[] args) {
